c588c3fc830aaaa38f5092f4ff0bd98d86c84839
mr.rob.byrne@gmail.com <mr.rob.byrne@gmail.com>
2014-10-29 00:28:56 -0400

    Updated LCS.java to include user input strings

--- Class codes/LCS.java
+++ Class codes/LCS.java
@@ -1,32 +1,42 @@
+import java.util.Scanner;
+
 class LCS {
 	public static void main(String[] args) {
-		String a = new String("robert");
-		String b = new String("byrne");
-		System.out.println("LCS: " + LCS(a,b));
-	}
-	public static int LCS(String a, String b) {
-		int m = a.length();
-		int n = b.length();
-		int [][] l = new int[m+1][n+1];
-		for(int i = 0; i<=m; i++)
-			l[i][0] = 0;
-		for(int j = 0; j<=n; j++)
-			l[0][j] = 0;
-		for (int i = 0; i<m; i++) {
-			for(int j = 0; j<n; j++) {
+		Scanner in = new Scanner(System.in);
+
+		System.out.println("Enter a string of letters: ");
+		String a = in.nextLine();
+		System.out.println("Enter another string of letters: ");
+		String b = in.nextLine();
+
+		int M = a.length();
+		int N = b.length();
+
+		
+		int [][] l = new int[M+1][N+1];
+		
+		for (int i = M-1; i >= 0; i--) {
+			for(int j = N-1; j >= 0; j--) {
 				if(a.charAt(i) == b.charAt(j))
-					l[i+1][j+1] = 1+l[i][j];
-			else {
-				l[i+1][j+1] = max(l[i+1][j], l[i][j+1]);
-			}
+					l[i][j] = l[i+1][j+1] + 1;
+				else {
+					l[i][j] = Math.max(l[i+1][j], l[i][j+1]);
+				}
 			}
 		}
-		return l[m][n];
-	}
-	public static int max(int x, int y) {
-		if(x>y)
-			return x;
-		else
-			return y;
+		int i = 0, j = 0;
+		while(i < M && j < N) {
+            if (a.charAt(i) == b.charAt(j)) {
+                System.out.print(a.charAt(i));
+                i++;
+                j++;
+            }
+            else if (l[i+1][j] >= l[i][j+1]) 
+            	i++;
+            else  
+            	j++;
+        }
+        System.out.println();
 	}
 }
+
